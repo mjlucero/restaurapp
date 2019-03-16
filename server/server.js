243d947f3d2de4b570e175e61baf7ca0752a8a22
mlucero <mauricio.lucero2014@gmail.com>
2019-03-16 19:50:28 -0300

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require('./config/config');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+ 
+app.get('/hello', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(process.env.PORT);
